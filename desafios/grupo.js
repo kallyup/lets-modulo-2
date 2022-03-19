afee53bef530f1c4062016218e69d73dfe47e484
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-03-18 21:48:39 -0300

    Update Aula 06

--- desafios/grupo.js
+++ desafios/grupo.js
@@ -3,7 +3,7 @@ const lancamentos = [];
 class Bank {
     #passcode;
     #balance;
-    constructor(name, account, balance, passcode = "Senha123", limite) {
+    constructor(name, account, balance, passcode = '123', limite) {
         this.name = name,
         this.account = account,
         this.#balance = balance,
@@ -45,6 +45,7 @@ class Bank {
         }
     }
 
+
     saque = (valor) => {
         if (valor <= this.#balance & this.#passcode){
             this.#balance -= valor;
@@ -52,10 +53,21 @@ class Bank {
             lancamentos.push(lancamento);
             return `Valor de R$ ${valor} Retirado com Sucesso | Saldo Atual = | R$ ${this.#balance}`;     
         }else if( valor>this.#balance & this.#passcode){
-            return "Saldo indisponivel";
+            const resposta = prompt('Saldo insuficiente. Gostaria de utilizar o cheque especial? \n1 - Sim\n2 - Não');
+            if(resposta == 1) {
+                const valorCheque = valor - this.#balance;
+                return this.Leasing(valor, valorCheque);
+            } else {
+                return "Saldo indisponivel";
+            }
         }else{
             return "Senha Incorreta" ;
         }
+    }
+    Leasing = (valor, valorCheque) => {
+        this.valor = valor;
+        // this.valorCheque = valorCheque;
+        this.saque(valor + valorCheque);
     }
 
     deposito = (valor ) => {
@@ -90,9 +102,7 @@ class Bank {
         }
     }
 
-    retornaDados = () => {
-
-    }
+    retornaDados = () => {}
 
     calculaJuros (valor, diasAtraso, taxaJuros = 2.5) {
         if (diasAtraso == 1) {
@@ -103,7 +113,6 @@ class Bank {
             return (valor*(1+taxaJuros/100)**diasAtraso - valor).toFixed(2);
         }
     }
-
 
 }
 
@@ -116,6 +125,10 @@ class PJ extends Bank{
     retornaDados = () => {
         return `CNPJ: ${this.cnpj}`
     }
+    Leasing = (valor) => {
+        console.log('ValorLeasing: ',typeof(valor));
+        return valor += parseFloat((valor*0.01).toFixed(2));
+    }
 }
 
 class PF extends Bank{
@@ -126,6 +139,10 @@ class PF extends Bank{
     }
     retornaDados = () => {
         return `CPF: ${this.cpf}\nRG: ${this.rg}`
+    }
+    Leasing = (valor) => {
+        console.log('ValorLeasing: ',typeof(valor));
+        return valor += parseFloat((valor*0.005).toFixed(2));
     }
 }
 
@@ -140,27 +157,28 @@ console.log(joao.saque(3000)); // Valor indisponível
 */
 
 console.log("=== Pessoa Fisica ===")
-const pessoa2 = new PF("Gabriel", 0001, 2000, "Senha123", '087.463.636-02', '123456-7');
+const pessoa2 = new PF("Gabriel", 0001, 2000, '123', '087.463.636-02', '123456-7');
 console.log(pessoa2.balance);
-console.log(pessoa2);
-console.log(pessoa2.conferirSenha());
-console.log(pessoa2.emprestimo(200));
-console.log(pessoa2.deposito(300));
-console.log(pessoa2.saque(100));
-console.log(pessoa2.transferencia(500));
-console.log(pessoa2.retornaDados());
-console.log(lancamentos);
-console.log(pessoa2.deposito(1000));
-console.log(lancamentos);
-console.log(pessoa2.calculaJuros(637, 1));
-console.log(pessoa2.calculaJuros(637, 2));
-console.log(pessoa2.calculaJuros(637, 3));
-console.log(pessoa2.calculaJuros(1000, 4, 3.5));
-console.log(pessoa2.calculaJuros(637, 4, 3.5));
+// console.log(pessoa2);
+// console.log(pessoa2.conferirSenha());
+// console.log(pessoa2.emprestimo(200));
+// console.log(pessoa2.deposito(300));
+// console.log(pessoa2.saque(100));
+// console.log(pessoa2.transferencia(500));
+// console.log(pessoa2.retornaDados());
+// console.log(lancamentos);
+// console.log(pessoa2.deposito(1000));
+// console.log(lancamentos);
+// console.log(pessoa2.calculaJuros(637, 1));
+// console.log(pessoa2.calculaJuros(637, 2));
+// console.log(pessoa2.calculaJuros(637, 3));
+// console.log(pessoa2.calculaJuros(1000, 4, 3.5));
+// console.log(pessoa2.calculaJuros(637, 4, 3.5));
+console.log(pessoa2.saque(1000));
+console.log(pessoa2.saque(3542));
 
-
-// console.log("=== Pessoa Juridica ===")
-// const pessoa3 = new PJ("Debora", 0002, 3000, "Senha12", 12332133);
+console.log("=== Pessoa Juridica ===")
+const pessoa3 = new PJ("Debora", 0002, 2000, '123', 12332133);
 // console.log(pessoa3.balance)
 // console.log(pessoa3);
 // console.log(pessoa3.conferirSenha());
@@ -169,3 +187,34 @@ console.log(pessoa2.calculaJuros(637, 4, 3.5));
 // console.log(pessoa3.emprestimo(100));
 // console.log(pessoa3.transferencia(500));
 // console.log(pessoa3.retornaDados());
+console.log(pessoa3.saque(1000));
+console.log(pessoa3.saque(3542));
+
+
+/* 
+EXERCÍCIOS AULA 06 - 18/03/2022
+
+- Crie um método de Leasing para o cliente
+considerando as seguintes condições:
+- Se cliente PJ, considerar Juros diário de 1%
+- Se cliente PF, considerar juros diário de 0.5 %
+
+- Sempre que o usuário for fazer um saque, pagar uma conta, ou realizar 
+transferência e o saldo disponível em conta for menor que o valor da operação,  
+verificar se o mesmo quer fazer uso do Leasing, se sim, liberar o saque e informar o 
+valor negativo e a taxa de juros a ser aplicada.
+
+- Implementar um método que calcule e armazene o saldo do juros do leasing da conta, 
+sendo separado por data, para ser possível ver o quanto de juros de leasing o cliente 
+esta acumulando diariamente.
+[Saldo negativo:
+Juros Diário:
+Data:
+Juros Total Acumulado:]
+
+OBS: Sempre que o cliente entrar no cheque especial o metodo precisa ser invocado
+
+- Crie um método que irá verificar se o cliente está com cheque especial, 
+e se ocorrer um depósito em conta, é necessário abater o valor do cheque especial
+
+*/
